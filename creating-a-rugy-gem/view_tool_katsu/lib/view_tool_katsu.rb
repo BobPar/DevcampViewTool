7b87d787fdc14d25530e7367a41adf63d21b5fa0
BobParr <sugada.katsuhiro@gmail.com>
2022-03-11 14:51:09 +0900

    Dev tool initial commit

---
+++ creating-a-rugy-gem/view_tool_katsu/lib/view_tool_katsu.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "view_tool_katsu/version"
+require_relative "view_tool_katsu/renderer"
+
+module ViewToolKatsu
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ creating-a-rugy-gem/view_tool_katsu/lib/view_tool_katsu/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ViewToolKatsu
+  VERSION = "0.1.0"
+end
